46134c4bccf88e9143bac6287bd0b27d262bdc2a
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-18 15:19:08 +0000

    Add test for user registration

---
+++ tests/test_users.py
@@ -0,0 +1,39 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+class TestUserRegistration:
+    """TestUserRegistration tests /users/register"""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        pass
+
+    def test_get_request_returns_405(self):
+        """registration endpoint does only expect a post request"""
+        response = client.get("/users/register")
+        assert response.status_code == 405
+
+    def test_post_request_without_body_returns_422(self):
+        """body should have username, password and fullname"""
+        response = client.post("/users/register")
+        assert response.status_code == 422
+
+    def test_post_request_with_improper_body_returns_422(self):
+        """all of username, password and fullname is required"""
+        response = client.post(
+            "/users/register",
+            json={"username": "santosh"}
+        )
+        assert response.status_code == 422
+
+    def test_post_request_with_proper_body_returns_201(self):
+        response = client.post(
+            "/users/register",
+            json={"username": "santosh", "password": "sntsh", "fullname": "Santosh Kumar"}
+        )
+        assert response.status_code == 201
