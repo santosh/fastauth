f5353e8893821a140531e1e5145d758879234fce
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-06 20:33:57 +0000

    Add spec for login

--- tests/test_users.py
+++ tests/test_users.py
@@ -54,3 +54,33 @@ class TestUserRegistration:
             json={"username": "santosh", "password": "sntsh", "fullname": "Santosh Kumar"}
         )
         assert response.status_code == 201
+
+
+class TestUserLogin:
+    """TestUserLogin tests /users/auth"""
+
+    def test_get_request_returns_405(self):
+        """login endpoint does only expect a post request"""
+        response = client.get("/users/auth")
+        assert response.status_code == 405
+
+    def test_post_request_without_body_returns_422(self):
+        """body should have username, password and fullname"""
+        response = client.post("/users/auth")
+        assert response.status_code == 422
+
+    def test_post_request_with_improper_body_returns_422(self):
+        """both username and password is required"""
+        response = client.post(
+            "/users/auth",
+            json={"username": "santosh"}
+        )
+        assert response.status_code == 422
+
+    def test_post_request_with_proper_body_returns_200_with_jwt_token(self):
+        response = client.post(
+            "/users/auth",
+            json={"username": "santosh", "password": "sntsh"}
+        )
+        assert response.status_code == 200
+        assert len(response.json()) == 2
